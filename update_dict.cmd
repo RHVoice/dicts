a7e3a26610a808b20ffd26fb57367e6311b78e71
ANT <velmyshanovnyi@gmail.com>
2020-05-24 16:17:33 +0300

    curl

--- update_dict.cmd
+++ update_dict.cmd
@@ -5,14 +5,25 @@ chcp 65001
 chcp 65001 > nul
 @echo #
 @echo # 
+
 @echo # C:\Users\####\AppData\Roaming\RHVoice\dicts\Ukrainian
 @echo # mkdir %AppData%\RHVoice\dicts\Russian
 @echo # mkdir %AppData%\RHVoice\dicts\Ukrainian
+set FolderTEMP=temp
+@echo # mkdir !FolderTEMP!
+mkdir !FolderTEMP!
 mkdir %AppData%\RHVoice\dicts\Russian
 mkdir %AppData%\RHVoice\dicts\Ukrainian
 @echo #
-@echo # wget https://github.com/RHVoice/dicts/archive/master.zip
-wget 
+@echo # curl -o "!FolderTEMP!\unzip.exe"        -LJO http://stahlworks.com/dev/unzip.exe
+@echo # curl -o "!FolderTEMP!\dicts-master.zip" -LJO https://github.com/RHVoice/dicts/archive/master.zip
+curl -o "!FolderTEMP!\unzip.exe" -LJO http://stahlworks.com/dev/unzip.exe
+curl -o "!FolderTEMP!\dicts-master.zip" -LJO https://github.com/RHVoice/dicts/archive/master.zip
+@echo #
+REM mkdir !FolderTEMP!\git
+@echo #
+@echo # !FolderTEMP!\unzip.exe "!FolderTEMP!\dicts-master.zip" "!FolderTEMP!"
+!FolderTEMP!\unzip.exe -l "!FolderTEMP!\dicts-master.zip" "!FolderTEMP!"
 @echo #
 @echo # 
 @echo #
