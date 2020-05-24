88b918ca944a75d56e75dbad5c1546fe8042a384
ANT <velmyshanovnyi@gmail.com>
2020-05-24 15:19:23 +0300

    Create update_dict.cmd

---
+++ update_dict.cmd
@@ -0,0 +1,30 @@
+setlocal EnableDelayedExpansion
+@echo off
+chcp
+chcp 65001
+chcp 65001 > nul
+@echo #
+@echo # 
+@echo # C:\Users\####\AppData\Roaming\RHVoice\dicts\Ukrainian
+@echo # mkdir %AppData%\RHVoice\dicts\Russian
+@echo # mkdir %AppData%\RHVoice\dicts\Ukrainian
+mkdir %AppData%\RHVoice\dicts\Russian
+mkdir %AppData%\RHVoice\dicts\Ukrainian
+@echo #
+@echo # wget https://github.com/RHVoice/dicts/archive/master.zip
+wget 
+@echo #
+@echo # 
+@echo #
+@echo # 
+@echo #
+@echo # 
+
+
+
+
+
+
+
+
+cmd -k
