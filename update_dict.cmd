ac477c08b672fb4a77058c2d3a3508ab034a31f3
ANT <velmyshanovnyi@gmail.com>
2021-01-18 03:45:05 +0200

    2021 BIG REFACTORING :)

--- update_dict.cmd
+++ update_dict.cmd
@@ -6,19 +6,18 @@ REM chcp 65001 > nul
 @echo #################################################
 @echo #
 @echo # This dictionary updater for RHVoice Speech Synthesizer
-@echo # (https://github.com/Olga-Yakovleva/RHVoice)
-@echo # OR (https://github.com/RHVoice)
+@echo # https://github.com/RHVoice
 @echo #
 @echo # This updater is distributed under LGPL v2.1 or later.
-@echo # Copyright (C) 2020 DictionaryTeam (https://github.com/RHVoice)
+@echo # Copyright (C) 2020-2021 DictionaryTeam (https://github.com/RHVoice/dicts)
 @echo # DictionaryTeam:
 @echo # ANT Berezhnyi # velmyshanovnyi@gmail.com # www.t.me/velmyshanovnyi #
 @echo #
 @echo # ----- WINDOWS -----
-@echo # 1) У вас вже має бути заінстальований реліз RHVoice. Брати тут:
-@echo # https://github.com/Olga-Yakovleva/RHVoice/releases
+@echo # 1) У вас має бути вже заiнстальований релiз RHVoice. Брати тут:
+@echo # https://github.com/RHVoice/RHVoice/releases
 @echo #
-@echo # 2) щоб оновити словники запустіть
+@echo # 2) щоб повторно оновлювати словники запускай
 @echo # "update_dict.cmd"
 @echo #
 @echo #################################################
@@ -42,39 +41,114 @@ rmdir /s/q %FolderTEMP%
 @echo #
 mkdir %FolderTEMP%
 @echo #
-@echo # "для профілактики" створюємо системні папки із словниками (в разі якщо їх нема).
-@echo # C:\Users\####\AppData\Roaming\RHVoice\dicts\Russian
-@echo # C:\Users\####\AppData\Roaming\RHVoice\dicts\Ukrainian
-@echo # mkdir %AppData%\RHVoice\dicts\Russian
-@echo # mkdir %AppData%\RHVoice\dicts\Ukrainian
+
 @echo #
-mkdir %AppData%\RHVoice\dicts\Russian
-mkdir %AppData%\RHVoice\dicts\Ukrainian
+@echo # "для профілактики" створюємо системні папки мов (на випадок їх відсутності) для словників (в разі якщо їх теж ще нема).
+@echo # C:\Users\#####\AppData\Roaming\RHVoice\dicts\LANG
+@echo # mkdir %AppData%\RHVoice\dicts\LANG
 @echo #
+mkdir "%AppData%\RHVoice\dicts\Brazilian-Portuguese"
+mkdir "%AppData%\RHVoice\dicts\English"
+mkdir "%AppData%\RHVoice\dicts\Esperanto"
+mkdir "%AppData%\RHVoice\dicts\Georgian"
+mkdir "%AppData%\RHVoice\dicts\Kyrgyz"
+mkdir "%AppData%\RHVoice\dicts\Russian"
+mkdir "%AppData%\RHVoice\dicts\Tatar"
+mkdir "%AppData%\RHVoice\dicts\Ukrainian"
+@echo #
+
+@echo #
+@echo # завантажуємо утиліту для консольної розпаковки
 @echo # curl -o "%FolderTEMP%\unzip.exe" -LJO http://stahlworks.com/dev/unzip.exe
+@echo # завантажуємо архів останної версії скрипта
 @echo # curl -o "%FolderTEMP%\dicts-master.zip" -LJO https://github.com/RHVoice/dicts/archive/master.zip
+@echo #
 curl -o "%FolderTEMP%\unzip.exe" -LJO http://stahlworks.com/dev/unzip.exe
 curl -o "%FolderTEMP%\dicts-master.zip" -LJO https://github.com/RHVoice/dicts/archive/master.zip
+@echo #
+
+@echo #
+@echo # створюємо папки мов в тимчасоввій папці "%FolderTEMP%"
+mkdir %FolderTEMP%\RHVoice\Brazilian-Portuguese
+mkdir %FolderTEMP%\RHVoice\English
+mkdir %FolderTEMP%\RHVoice\Esperanto
+mkdir %FolderTEMP%\RHVoice\Georgian
+mkdir %FolderTEMP%\RHVoice\Kyrgyz
+mkdir %FolderTEMP%\RHVoice\Russian
+mkdir %FolderTEMP%\RHVoice\Tatar
+mkdir %FolderTEMP%\RHVoice\Ukrainian
+@echo #
+
+@echo #
+@echo # завантажуємо системні/дефолтові словники для мов із https://github.com/RHVoice
+@echo # сторінки наявних словників:
+@echo # https://github.com/RHVoice/Brazilian-Portuguese/blob/main/userdict/src/dict.txt
+@echo # https://github.com/RHVoice/Ukrainian/blob/main/userdict/src/dictionary.txt
+@echo # що фізично знаходяться тут:
+@echo # https://raw.githubusercontent.com/RHVoice/Brazilian-Portuguese/main/userdict/src/dict.txt
+@echo # https://raw.githubusercontent.com/RHVoice/Ukrainian/main/userdict/src/dictionary.txt
+@echo # 
+curl -o "%FolderTEMP%\RHVoice\Brazilian-Portuguese\dict_pt.txt" -LJO https://raw.githubusercontent.com/RHVoice/Brazilian-Portuguese/main/userdict/src/dict.txt
+curl -o "%FolderTEMP%\RHVoice\English\dict_en.txt"   -LJO https://raw.githubusercontent.com/RHVoice/English/main/userdict/src/dict.txt
+curl -o "%FolderTEMP%\RHVoice\Esperanto\dict_eo.txt" -LJO https://raw.githubusercontent.com/RHVoice/Esperanto/main/userdict/src/dict.txt
+curl -o "%FolderTEMP%\RHVoice\Georgian\dict_ge.txt"  -LJO https://raw.githubusercontent.com/RHVoice/Georgian/main/userdict/src/dict.txt
+curl -o "%FolderTEMP%\RHVoice\Kyrgyz\dict_ky.txt"    -LJO https://raw.githubusercontent.com/RHVoice/Kyrgyz/main/userdict/src/dict.txt
+curl -o "%FolderTEMP%\RHVoice\Russian\dict_ru.txt"   -LJO https://raw.githubusercontent.com/RHVoice/Russian/main/userdict/src/dict.txt
+curl -o "%FolderTEMP%\RHVoice\Tatar\dict_tt.txt"     -LJO https://raw.githubusercontent.com/RHVoice/Tatar/main/userdict/src/dict.txt
+curl -o "%FolderTEMP%\RHVoice\Ukrainian\dict_uk.txt" -LJO https://raw.githubusercontent.com/RHVoice/Ukrainian/main/userdict/src/dictionary.txt
+@echo #
+
 @echo #
 @echo # розархівовуємо словники в тимчасову папку: %FolderTEMP%
 @echo # %FolderTEMP%\unzip.exe "%FolderTEMP%\dicts-master.zip" -d "%FolderTEMP%"
 @echo #
 %FolderTEMP%\unzip.exe "%FolderTEMP%\dicts-master.zip" -d "%FolderTEMP%"
 @echo #
+
 @echo #
-@echo # переміщуємо словникм із тимчасової папки в системну
-@echo # move /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\dicts\LANG\*.*" "%AppData%\RHVoice\dicts\LANG"
+@echo # копіюємо завантажені словники із тимчасової папки в системну:
+@echo # copy /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\dicts\LANG\*.txt" "%AppData%\RHVoice\dicts\LANG"
 @echo #
-move /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\dicts\Russian\*.*" "%AppData%\RHVoice\dicts\Russian"
-move /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\dicts\Ukrainian\*.*" "%AppData%\RHVoice\dicts\Ukrainian"
+copy /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\dicts\Brazilian-Portuguese\*.txt" "%AppData%\RHVoice\dicts\Brazilian-Portuguese"
+copy /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\dicts\English\*.txt" "%AppData%\RHVoice\dicts\English"
+copy /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\dicts\Esperanto\*.txt" "%AppData%\RHVoice\dicts\Esperanto"
+copy /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\dicts\Georgian\*.txt" "%AppData%\RHVoice\dicts\Georgian"
+copy /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\dicts\Kyrgyz\*.txt" "%AppData%\RHVoice\dicts\Kyrgyz"
+copy /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\dicts\Russian\*.txt" "%AppData%\RHVoice\dicts\Russian"
+copy /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\dicts\Tatar\*.txt" "%AppData%\RHVoice\dicts\Tatar"
+copy /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\dicts\Ukrainian\*.txt" "%AppData%\RHVoice\dicts\Ukrainian"
+@echo # 
+
 @echo #
-@echo # Перевірка конфігураційних файлів, якщо нема - створити, якщо є - залишити такими як є.
+@echo # УВАГА! ================================================================================ УВАГА! 
+@echo # УВАГА! РОЗКОМЕНТУВАТИ в коді цього файла потрібний рядок мови ДЛЯ ЛОКАЛЬНОІ ВІДЛАДКИ!
+@echo # УВАГА! при фазі оновення скрипта, розкоментований рядок буде заменено на ЗАКОМЕНТОВАНИЙ
+@echo # УВАГА! це зроблено для безпеки!
+@echo # УВАГА! ================================================================================ УВАГА!  
+@echo # копіюємо словникм із локальної папки в системну (для тих випадків, коли словник наповнюється та правиться локально частіше ніж на Гіті)
+@echo # copy /y "LANG\*.txt" "%AppData%\RHVoice\dicts\LANG"
+@echo #
+REM copy /y "Brazilian-Portuguese\*.txt" "%AppData%\RHVoice\dicts\Brazilian-Portuguese"
+REM copy /y "English\*.txt"              "%AppData%\RHVoice\dicts\English"
+REM copy /y "Esperanto\*.txt"            "%AppData%\RHVoice\dicts\Esperanto"
+REM copy /y "Georgian\*.txt"             "%AppData%\RHVoice\dicts\Georgian"
+REM copy /y "Kyrgyz\*.txt"               "%AppData%\RHVoice\dicts\Kyrgyz"
+REM copy /y "Russian\*.txt"              "%AppData%\RHVoice\dicts\Russian"
+REM copy /y "Tatar\*.txt"                "%AppData%\RHVoice\dicts\Tatar"
+REM copy /y "Ukrainian\*.txt"            "%AppData%\RHVoice\dicts\Ukrainian"
+@echo #
+
+
+@echo #
+@echo # Перевірка конфігураційних файлів, якщо нема - створити (скопіювати дефолтові), якщо є - залишити такими як є.
 @echo #
 @echo # RHVoice.conf
 @echo # RHVoice.ini
-if not exist "%AppData%\RHVoice\RHVoice.conf" ( move /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\RHVoice.conf" "%AppData%\RHVoice" )
-if not exist "%AppData%\RHVoice\RHVoice.ini"  ( move /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\RHVoice.ini"  "%AppData%\RHVoice" )
+if not exist "%AppData%\RHVoice\RHVoice.conf" ( copy /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\RHVoice.conf" "%AppData%\RHVoice" )
+if not exist "%AppData%\RHVoice\RHVoice.ini"  ( copy /y "%FolderTEMP%\dicts-master\AppData\Roaming\RHVoice\RHVoice.ini"  "%AppData%\RHVoice" )
 @echo #
+
+
 @echo #
 @echo # оновлення версії самого скрипта
 @echo # if not exist "%FolderTEMP%\update_script.cmd" ( goto :updateScriptA )
@@ -92,7 +166,7 @@ rmdir /s/q %FolderTEMP%
 @echo # ## (якщо не оновилось не все, запусти повторно)     ##
 @echo # ##                                                  ##
 @echo # ## ------------------------------------------------ ##
-@echo # ## версiя скрипта вiд : 20200701-0001               ##
+@echo # ## версiя скрипта вiд : 20210118-0002               ##
 @echo # ## версiя словникiв   : по датi оновлення на GitHub ##
 @echo # ## https://github.com/RHVoice/dicts                 ##
 @echo # ## ------------------------------------------------ ##
